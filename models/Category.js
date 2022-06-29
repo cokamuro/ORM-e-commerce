cea5259e61f1058f34f3c16c86cdb8ecd5d14848
Chris Okamuro <cokamuro@gmail.com>
2022-06-29 15:59:44 -0400

    added category and product models

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,18 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    // id, int, no null, pk, auto inc
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // category_name, string, no null
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }    
   },
   {
     sequelize,
